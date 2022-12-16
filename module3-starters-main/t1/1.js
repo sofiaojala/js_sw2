5eafd13f8ccc4a8c50ebbc2bfe32a7a204638db9
Sofia <ssofiaojala@gmail.com>
2022-12-16 15:33:06 +0200

    lol

---
+++ module3-starters-main/t1/1.js
@@ -0,0 +1,5 @@
+'use strict'
+let html = document.getElementById("target").innerHTML +=
+            "<li>First Item</li><li>Second Item</li><li>Third Item</li>"
+let x = document.getElementById("target")
+x.className+='mylist';
